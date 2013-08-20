0ac2b7d0b96094012734dedf184e370be8180ca7
Suguru Hamazaki <hamazy@webspiritus.com>
2013-08-20 22:21:59 +0900

    *** empty log message ***

--- install-ensime.sh
+++ install-ensime.sh
@@ -9,3 +9,17 @@ if [ ! -d $ENSIME_ROOT ] ; then
     curl -s -L https://www.dropbox.com/sh/ryd981hq08swyqr/ZiCwjjr_vm/ENSIME%20Releases/ensime_2.10.0-0.9.8.9.tar.gz | tar -xzf - --strip-components 1 -C $ENSIME_ROOT
     chmod 755 $ENSIME_ROOT/bin/server
 fi
+
+[ ! -d $HOME/.sbt/plugins ] && mkdir -p $HOME/.sbt/plugins
+if [ ! $(grep "ensime-sbt-cmd" $HOME/.sbt/plugins/plugins.sbt) ] ; then
+    ed <<EOF
+f $HOME/.sbt/plugins/plugins.sbt
+\$a
+
+addSbtPlugin("org.ensime" % "ensime-sbt-cmd" % "0.1.1")
+
+.
+w
+q
+EOF
+fi
