e43f6e79be81510e9aaceb629c70c88fc57af3a0
Suguru Hamazaki <hamazy@webspiritus.com>
2013-08-20 22:09:53 +0900

    *** empty log message ***

--- install-ensime.sh
+++ install-ensime.sh
@@ -6,6 +6,6 @@ set -o nounset
 ENSIME_ROOT=$HOME/.ensime
 if [ ! -d $ENSIME_ROOT ] ; then
     mkdir $ENSIME_ROOT
+    curl -s -L https://www.dropbox.com/sh/ryd981hq08swyqr/ZiCwjjr_vm/ENSIME%20Releases/ensime_2.10.0-0.9.8.9.tar.gz | tar -xzf - --strip-components 1 -C $ENSIME_ROOT
+    chmod 755 $ENSIME_ROOT/bin/server
 fi
-curl -s -L https://www.dropbox.com/sh/ryd981hq08swyqr/ZiCwjjr_vm/ENSIME%20Releases/ensime_2.10.0-0.9.8.9.tar.gz | tar -xzf - --strip-components 1 -C $ENSIME_ROOT
-chmod 755 $ENSIME_ROOT/bin/server
---
+++ install-sbt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+SBT_VERSION=0.12.1
+SBT_PACKAGE_DIR=$HOME/.sbt-$SBT_VERSION
+
+if [ ! -d $SBT_PACKAGE_DIR ] ; then
+    curl -s -L http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/$SBT_VERSION/sbt.tgz | tar -xzf - -C $SBT_PACKAGE_DIR
+fi
+
