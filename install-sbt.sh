7829620de3318f120b106f224ebecd59ebc72e92
Suguru Hamazaki <hamazy@webspiritus.com>
2013-08-20 22:12:44 +0900

    *** empty log message ***

--- install-sbt.sh
+++ install-sbt.sh
@@ -8,6 +8,6 @@ SBT_PACKAGE_DIR=$HOME/.sbt-$SBT_VERSION
 
 if [ ! -d $SBT_PACKAGE_DIR ] ; then
     mkdir $SBT_PACKAGE_DIR
-    curl -s -L http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/$SBT_VERSION/sbt.tgz | tar -xzf - -C $SBT_PACKAGE_DIR
+    curl -s -L http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/$SBT_VERSION/sbt.tgz | tar -xzf - --strip-components 1 -C $SBT_PACKAGE_DIR
 fi
 
