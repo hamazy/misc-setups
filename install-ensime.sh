b79d742b8a12dfcbbbee587c31be6a7db2373ee7
Suguru Hamazaki <hamazy@webspiritus.com>
2013-08-20 21:22:09 +0900

    *** empty log message ***

--- install-ensime.sh
+++ install-ensime.sh
@@ -7,5 +7,5 @@ ENSIME_ROOT=$HOME/.emsime
 if [ ! -d $ENSIME_ROOT ] ; then
     mkdir $ENSIME_ROOT
 fi
-curl -s -L https://www.dropbox.com/sh/ryd981hq08swyqr/ZiCwjjr_vm/ENSIME%20Releases/ensime_2.10.0-0.9.8.9.tar.gz | tar -xzf - -C $ENSIME_ROOT
+curl -s -L https://www.dropbox.com/sh/ryd981hq08swyqr/ZiCwjjr_vm/ENSIME%20Releases/ensime_2.10.0-0.9.8.9.tar.gz | tar -xzf - --strip-components 1 -C $ENSIME_ROOT
 chmod 755 $ENSIME_ROOT/bin/server
