75a5146128f252eae624d2260e58a2f8ef53dabe
Suguru Hamazaki <hamazy@webspiritus.com>
2013-11-01 10:04:36 +0900

    Add for lein.

---
+++ install-lein.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+[ ! -d $HOME/.lein/bin ] && mkdir -p $HOME/.lein/bin
+[ ! -f $HOME/.lein/bin/lein ] && curl https://raw.github.com/technomancy/leiningen/stable/bin/lein -o $HOME/.lein/bin/lein && chmod 755 $HOME/.lein/bin/lein
+# [ -x $HOME/.lein/bin/lein ] && export PATH=$HOME/.lein/bin:$PATH
