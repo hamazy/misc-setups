bc8dbaf9156817ccefe3f1fe363c238d5834b1b5
Suguru Hamazaki <hamazy@webspiritus.com>
2014-02-01 11:42:59 +0900

    Adopt for the directory strutcture of recent version of sbt.

--- install-ensime.sh
+++ install-ensime.sh
@@ -10,7 +10,8 @@ if [ ! -d $ENSIME_ROOT ] ; then
     chmod 755 $ENSIME_ROOT/bin/server
 fi
 
-SBT_PLUGINS=$HOME/.sbt/plugins
+SBT_VERSION=0.13
+SBT_PLUGINS=$HOME/.sbt/$SBT_VERSION/plugins
 SBT_PLUGINS_DEF=$SBT_PLUGINS/plugins.sbt
 [ ! -d $SBT_PLUGINS ] && mkdir -p $SBT_PLUGINS
 [ ! -f $SBT_PLUGINS_DEF ] && touch $SBT_PLUGINS_DEF
@@ -20,7 +21,7 @@ if [ ! $? ] ; then
 f $HOME/.sbt/plugins/plugins.sbt
 \$a
 
-addSbtPlugin("org.ensime" % "ensime-sbt-cmd" % "0.1.1")
+addSbtPlugin("org.ensime" % "ensime-sbt-cmd" % "0.1.2")
 
 .
 w
