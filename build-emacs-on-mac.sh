e46a11d00dfc3b11788246f9a496896341d41160
Suguru Hamazaki <hamazy@webspiritus.com>
2014-03-11 22:36:28 +0900

    Clone from github

--- build-emacs-on-mac.sh
+++ build-emacs-on-mac.sh
@@ -9,20 +9,27 @@ while [ -h "$PRG" ] ; do
 done
 dir=$(cd "$(dirname "$PRG")"; pwd)
 
+version=24.3
+
 pushd "$dir" >/dev/null
 
-curl -s -L -O http://core.ring.gr.jp/pub/GNU/emacs/emacs-24.3.tar.gz
-tar -xzf emacs-24.3.tar.gz
+# curl -s -L -O http://core.ring.gr.jp/pub/GNU/emacs/emacs-${version}.tar.gz
+# tar -xzf emacs-${version}.tar.gz
 
-pushd emacs-24.3
+[ ! -d emacs-${version} ] && git clone git@github.com:mirrors/emacs.git emacs-${version}
+
+pushd emacs-${version}
+[ ! -f configure ] && ./autogen.sh
 CC=/usr/bin/gcc CXX=/usr/bin/g++ ./configure --with-ns
+make bootstrap
+make
 make install
 popd
 
 curl -s -L -O http://mew.org/Release/mew-6.5.tar.gz
 tar -xzf mew-6.5.tar.gz
 pushd mew-6.5
-./configure --prefix=$dir/emacs-24.3/nextstep/Emacs.app/Contents/MacOS/ --with-emacs=$dir/emacs-24.3/nextstep/Emacs.app/Contents/MacOS/Emacs --with-elispdir=$dir/emacs-24.3/nextstep/Emacs.app/Contents/Resources/site-lisp/ --with-etcdir=$dir/emacs-24.3/nextstep/Emacs.app/Contents/Resources/etc/ --infodir=$dir/emacs-24.3/nextstep/Emacs.app/Contents/Resources/info
+./configure --prefix=$dir/emacs-${version}/nextstep/Emacs.app/Contents/MacOS/ --with-emacs=$dir/emacs-${version}/nextstep/Emacs.app/Contents/MacOS/Emacs --with-elispdir=$dir/emacs-${version}/nextstep/Emacs.app/Contents/Resources/site-lisp/ --with-etcdir=$dir/emacs-${version}/nextstep/Emacs.app/Contents/Resources/etc/ --infodir=$dir/emacs-${version}/nextstep/Emacs.app/Contents/Resources/info
 make
 make install
 popd
@@ -31,8 +38,8 @@ curl -s -L -O http://www.ring.gr.jp/archives/elisp/skk/maintrunk/ddskk-15.1.tar.
 tar -xzf ddskk-15.1.tar.gz
 pushd ddskk-15.1
 curl -s -L -o - http://openlab.jp/skk/dic/SKK-JISYO.L.gz | gzip -dc >dic/SKK-JISYO.L
-make what-where EMACS=$dir/emacs-24.3/nextstep/Emacs.app/Contents/MacOS/Emacs
-make install EMACS=$dir/emacs-24.3/nextstep/Emacs.app/Contents/MacOS/Emacs
+make what-where EMACS=$dir/emacs-${version}/nextstep/Emacs.app/Contents/MacOS/Emacs
+make install EMACS=$dir/emacs-${version}/nextstep/Emacs.app/Contents/MacOS/Emacs
 popd
 if [ ! -f $HOME/.skk ] ; then
     ed <<EOF
