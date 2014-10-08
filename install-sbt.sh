d9cdb43e19c4886ce28bed42e4366ee89be7f8cf
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2014-10-08 02:47:52 +0000

    Upgrade sbt.

--- install-sbt.sh
+++ install-sbt.sh
@@ -3,11 +3,9 @@
 set -o errexit
 set -o nounset
 
-SBT_VERSION=0.13.1
-SBT_PACKAGE_DIR=$HOME/.sbt-$SBT_VERSION
+SBT_PACKAGE_DIR=$HOME/.sbt-${SBT_VERSION:=0.13.6}
 
 if [ ! -d $SBT_PACKAGE_DIR ] ; then
     mkdir $SBT_PACKAGE_DIR
-    curl -s -L http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/$SBT_VERSION/sbt.tgz | tar -xzf - --strip-components 1 -C $SBT_PACKAGE_DIR
+    curl -s -L https://dl.bintray.com/sbt/native-packages/sbt/${SBT_VERSION}/sbt-${SBT_VERSION}.tgz | tar -xzf - --strip-components 1 -C $SBT_PACKAGE_DIR
 fi
-
